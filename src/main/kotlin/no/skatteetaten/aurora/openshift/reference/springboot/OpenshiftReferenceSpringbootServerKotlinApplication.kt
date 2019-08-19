791bd7a1f7178871787c1b541aeb4b2fe42a1d21
Bent André Solheim <BentAndre.Solheim@Skatteetaten.no>
2019-08-19 11:49:52 +0200

    Initial import after start.spring.io

---
+++ src/main/kotlin/no/skatteetaten/aurora/openshift/reference/springboot/OpenshiftReferenceSpringbootServerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package no.skatteetaten.aurora.openshift.reference.springboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenshiftReferenceSpringbootServerKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenshiftReferenceSpringbootServerKotlinApplication>(*args)
+}
